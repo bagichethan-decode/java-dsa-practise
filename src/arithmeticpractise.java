5bf62f635c31bb75445bcb4a1d433a0f07fa86f0
Chethan Bagi <bagichethan@gmail.com>
2026-07-25 14:02:01 +0530

    practised java arithmetic.

---
+++ src/arithmeticpractise.java
@@ -0,0 +1,9 @@
+public class arithmeticpractise {
+    public static void main(String args[]) {
+        int x =89;
+        int y =98;
+        int z =90;
+        int result = (x+y+z);
+        System.out.println(result);
+    }
+}
---
+++ src/arithmeticprogram2.java
@@ -0,0 +1,10 @@
+public class arithmeticprogram2 {
+    public static void main(String args[]){
+        int length =10;
+        int bredth =5;
+        int area =length*bredth;
+        int perimeter = 2*(length+bredth);
+        System.out.println(area);
+        System.out.println(perimeter);
+    }
+}
