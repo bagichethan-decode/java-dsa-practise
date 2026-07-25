d28e5b0885d1778d6c95e03bbeaadec19f4ad38a
Chethan Bagi <bagichethan@gmail.com>
2026-07-25 14:03:40 +0530

    practised java arithmetic.

--- src/operators.java
+++ src/operators.java
@@ -1,12 +1,14 @@
 public class operators {
     public static void main(String args[])
     {
-       int a =5;
-       int b =4;
-       int i =9;
-       int z =8;
+//       int a =5;
+//       int b =4;
+//       int i =9;
+//       int z =8;
+//
+//       boolean result = (a>b && i>z);
+//       System.out.println(result);
 
-       boolean result = (a>b && i>z);
-       System.out.println(result);
+
     }
 }
