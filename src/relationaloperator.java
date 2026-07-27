ced9735cb7349f89b348a0176a0a146a6eb00cbd
Chethan Bagi <bagichethan@gmail.com>
2026-07-27 18:08:41 +0530

    relatinaloperator

---
+++ src/relationaloperator.java
@@ -0,0 +1,14 @@
+public class relationaloperator {
+    public class Relational1 {
+        public static void main(String[] args) {
+
+            int a = 15;
+            int b = 20;
+
+            System.out.println(a > b);
+            System.out.println(a < b);
+            System.out.println(a == b);
+            System.out.println(a != b);
+        }
+    }
+}
