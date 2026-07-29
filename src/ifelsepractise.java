a815413ecbf9a77dcc90dddb3937c263064eed22
Chethan Bagi <bagichethan@gmail.com>
2026-07-29 16:24:03 +0530

    if elseif else practised.java

---
+++ src/ifelsepractise.java
@@ -0,0 +1,23 @@
+public class ifelsepractise {
+    public static void main(String[] args) {
+
+        int a = 85;
+
+        if ( a  >= 90) {
+            System.out.println(a);
+        }
+        else if ( a>=80 &&  a<80) {
+            System.out.println("b");
+        }
+        else if ( a>=70 &&  a<90) {
+            System.out.println("c");
+        }
+        else if ( a>=60 &&  a<70) {
+            System.out.println("d");
+        }
+        else {
+                    System.out.println("F");
+            }
+        }
+    }
+
--- src/operatorprecedence2.java
+++ src/operatorprecedence2.java
@@ -26,3 +26,4 @@ public class operatorprecedence2 {
         }
     }
 
+
