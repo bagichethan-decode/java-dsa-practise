fd984ab5b2f0b8064db2bbc261d5567bac095b46
Chethan Bagi <bagichethan@gmail.com>
2026-07-30 13:38:03 +0530

    practised Switch operator.java

---
+++ src/switchstatement.java
@@ -0,0 +1,33 @@
+public class switchstatement {
+    public static void main(String[] args)
+    {
+        int z =5;
+
+        switch(z){
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("enter the number of day");
+        }
+
+    }
+}
