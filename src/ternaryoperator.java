15ffd75a751a672b7ca620147fb7c808572a07e7
Chethan Bagi <bagichethan@gmail.com>
2026-07-30 13:00:21 +0530

    practised ternaryoperator and( ?: ).java

---
+++ src/ternaryoperator.java
@@ -0,0 +1,18 @@
+public class ternaryoperator {
+    public static void main (String[] args){
+        int p = 5;
+        int result = 0;
+
+//        if (p%5==0)
+//            result = 102;
+//        else
+//            result = 210;
+
+// using of ?: instead of if else just for fu learning cant use everytime but yes sounds good
+        result = p%4==0 ? 10 : 20;
+         System.out.println(result);
+
+
+
+    }
+}
