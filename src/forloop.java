7956208fc5dd83be549e88d7307602e63800f838
Chethan Bagi <bagichethan@gmail.com>
2026-07-31 00:44:04 +0530

    practised FOR loop (i=0;i<n;i++).java

---
+++ src/forloop.java
@@ -0,0 +1,15 @@
+public class forloop
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 7; i++)
+        {
+                    System.out.println(j+8+" - " + (j+9));
+           }
+
+
+        }
+    }
+}
+
+
