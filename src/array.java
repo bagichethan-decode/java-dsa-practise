828fdaedf8ca69c5dd66ae39aab02b0a27dcf122
Chethan Bagi <bagichethan@gmail.com>
2026-07-31 14:14:51 +0530

    practised array nums.java

---
+++ src/array.java
@@ -0,0 +1,16 @@
+public class array
+{
+    public static void main(String[] args)
+    {
+        int nums[] = new int[5];
+        nums[0] = 4;
+        nums[1] = 5;
+        nums[2] = 18;
+        nums[3] = 45;
+        nums[4] = 7;
+        for (int i = 0; i <= 4; i++)
+        {
+            System.out.println(nums[i]);
+        }
+    }
+}
--- src/loops.java
+++ src/loops.java
@@ -2,9 +2,10 @@ public class loops {
     public static void main(String[] args) {
 
         int i = 1;
-
         while (i <= 4) {
             System.out.println("hi" + i);
+            i++;
+
             int j = 1;
             while (j <= 3) {
                 System.out.println("bye" + j);
@@ -12,7 +13,7 @@ public class loops {
             }
 
 
-            i++;
+
 
             System.out.println("bye" + i);
         }
