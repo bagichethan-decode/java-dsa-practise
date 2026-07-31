38454413e9a100bd93d65a483d292eb6bbc6e60b
bagichethan-decode <bagichethan@gmail.com>
2026-07-31 13:01:46 +0530

    Enable printing of even numbers from 2 to 50
    Uncommented the loop to print even numbers from 2 to 50.

--- src/evennumbers.java
+++ src/evennumbers.java
@@ -1,11 +1,15 @@
 public class evennumbers {
     static void main(String[] args) {
-//        for
-//            (int i=2;i<=50;i=i+2) {
-//            System.out.println(i);
-//        }
 
+        
+        for
+           (int i=2;i<=50;i=i+2) {
+          System.out.println(i);
+        }
 
+// from here it is aeven numers problem usingthe for loop
+
+        
         int sum = 0;
 
         for (int i = 1; i <= 10; i++) {
