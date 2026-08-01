44631ae0eafb08d6b9c0e620320b63262aefbb03
Chethan Lakshman bagi <bagichethan@gmail.com>
2026-08-01 15:11:56 +0530

    practised 2 sum array(min - max) example.java

--- src/twosum.java
+++ src/twosum.java
@@ -1,24 +1,27 @@
 public class twosum {
-    public static void main(String [] args){
-        int []arr = {3,2,9,5};
-        int max = arr[0];
-        int min = arr[0];
+        public static void main(String [] args){
+            int []arr = {3,2,9,5};
+            int max = arr[0];
+            int min = arr[0];
 
 
-        //i < arr.length
-        //arr.length tells how many elements are in the array.
+            //i < arr.length
+            //arr.length tells how many elements are in the array.
 
-        for (int i=0;i<arr.length;i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+            for (int i=0;i<arr.length;i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
             }
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
             System.out.println("max="+max);
             System.out.println("min="+min);
 
         }
 
     }
+
+
+}
