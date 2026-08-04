9f1c096390d26cb33dc31f062ff12db789cbd5ea
bagichethan-decode <bagichethan@gmail.com>
2026-08-04 17:44:55 +0530

    StringBuilder examples in string_builder.java

--- src/string_builder.java
+++ src/string_builder.java
@@ -1,19 +1,19 @@
 public class string_builder {
     public static void main(String args[]){
 
-//        StringBuilder Sb = new  StringBuilder ("java" );
-//        Sb.append("DSA");
-//        System.out.println(Sb);
+       StringBuilder Sb = new  StringBuilder ("java" );
+       Sb.append("DSA");
+       System.out.println(Sb);
+---------------------------------------------------------------------
+       StringBuilder Sb = new  StringBuilder (" chethan"  );
+       Sb.append(" DSA ");
+       Sb.insert(8," love ");
+       System.out.println(Sb);
 
-//        StringBuilder Sb = new  StringBuilder (" chethan"  );
-//        Sb.append(" DSA ");
-//        Sb.insert(8," love ");
-//        System.out.println(Sb);
-
-
-//        StringBuilder Sb = new StringBuilder ("programming");
-//        Sb.delete(3,7);
-//        System.out.println(Sb);
+---------------------------------------------------------------------
+       StringBuilder Sb = new StringBuilder ("programming");
+       Sb.delete(3,7);
+       System.out.println(Sb);
 
 
 
