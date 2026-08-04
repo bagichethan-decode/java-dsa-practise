a2c45cfb06d31000ac34c777045abb94cf699b22
Chethan Lakshman bagi <bagichethan@gmail.com>
2026-08-04 18:34:47 +0530

    String builder reversing the string builder  example.java

---
+++ src/String_builder2.java
@@ -0,0 +1,15 @@
+public class String_builder2 {
+    public static void main(String args[]) {
+
+
+        StringBuilder Sb = new StringBuilder (" ");
+        Sb.append  ("ana,cy,bo");
+        Sb.reverse();
+        Sb.reverse();
+        System.out.println(Sb);
+
+// THIS IS FREAKING ONE OF THE MOST IMPORTANT AND GOOD PROGRAM IT LOOKS LIKE GOOD THOUGH
+
+
+    }
+}
