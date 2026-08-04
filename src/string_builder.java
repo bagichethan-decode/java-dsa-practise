dd2487add5f817cb102a7ca0f7909063e2386bd1
Chethan Lakshman bagi <bagichethan@gmail.com>
2026-08-04 17:42:21 +0530

    String builder example.java

---
+++ src/string_builder.java
@@ -0,0 +1,25 @@
+public class string_builder {
+    public static void main(String args[]){
+
+//        StringBuilder Sb = new  StringBuilder ("java" );
+//        Sb.append("DSA");
+//        System.out.println(Sb);
+
+//        StringBuilder Sb = new  StringBuilder (" chethan"  );
+//        Sb.append(" DSA ");
+//        Sb.insert(8," love ");
+//        System.out.println(Sb);
+
+
+//        StringBuilder Sb = new StringBuilder ("programming");
+//        Sb.delete(3,7);
+//        System.out.println(Sb);
+
+
+
+
+
+    }
+}
+
+
