bf571084789a7fd16c5e262c3a17b4d75d9dc64e
Chethan Lakshman bagi <bagichethan@gmail.com>
2026-08-07 20:48:11 +0530

    Added encapsulation example using Human class.java

---
+++ src/human.java
@@ -0,0 +1,27 @@
+public class human {
+    //CONSIDER IT AS A PRIVATE DATA (PRIVACY)
+    private String name;
+    private int age;
+
+    public static void main(String args[]) {
+
+        human h1 = new human();
+
+        h1.introduce("chethan",20);
+        h1.showdetails();
+    }
+
+    //METHOD TO SET OR GET
+    public void introduce(String n,int a) {
+        name = n;
+        age = a;
+    }
+    //METHOD :- SO THAT YOU CAN ACCESS THE DATA
+    public void showdetails(){
+        System.out.println("name:"+name);
+        System.out.println("age:"+age);
+        }
+    }
+
+
+
