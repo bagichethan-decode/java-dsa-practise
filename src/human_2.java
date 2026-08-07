49bc06ca3d5af22cd91be7e98654893a23dff99a
Chethan Lakshman bagi <bagichethan@gmail.com>
2026-08-07 22:07:03 +0530

    getter and setter example.java

--- src/human_2.java
+++ src/human_2.java
@@ -1,37 +1,35 @@
 public class human_2 {
-        //PRIVATE DATA
-        private String name;
-        private int age;
+
+    // PRIVATE DATA
+    private String name;
+    private int age;
 
     public static void main(String args[]) {
 
         human_2 h2 = new human_2();
 
-        //SETTER
+        // SETTER
         h2.setName("chethan");
         h2.setAge(20);
 
-        //GETTER
+        // GETTER
         System.out.println("Name:" + h2.getName());
         System.out.println("Age:" + h2.getAge());
     }
 
-        public String getName() {
-          return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAge(){
-            return age;
-        }
-
-        public void setAge(int age){
-            this.age = age;
-        }
-
-
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
