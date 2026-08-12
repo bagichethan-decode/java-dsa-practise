1d983c39a5963d219586f8d8b09508cfb2d56a48
Chethan Lakshman Bagi <bagichethan@gmail.com>
2026-08-12 15:27:55 +0530

    LinkedList basics: Node, head, next and traversal

---
+++ src/LinkedList_1.java
@@ -0,0 +1,36 @@
+public class LinkedList_1 {
+    public static void main(String args[]){
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        Node fourth = new Node(40);
+
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+
+        Node head = first;
+
+        Node current = head;
+
+        while (current != null) {
+            System.out.print(current.data + " ->");
+            current = current.next;
+        }
+        System.out.print("null");
+
+    }
+
+    static class Node {
+        int data;
+        Node next;
+
+        //constructor
+        Node(int data){
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+}
