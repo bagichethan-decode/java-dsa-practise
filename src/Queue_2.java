2963f684ed5d616d74075aacae3d4cf67d90bd4e
Chethan Lakshman Bagi <bagichethan@gmail.com>
2026-08-16 22:10:32 +0530

    queue question  example

---
+++ src/Queue_2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Queue_2 {
+
+    public static void main(String args[]) {
+        Queue<Integer> queue = new ArrayDeque<>();
+
+
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+        queue.offer(40);
+
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
+}
+
