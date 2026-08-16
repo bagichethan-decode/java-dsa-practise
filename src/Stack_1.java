84d8644edbbd5041bb3c2a40dac287f16e15a195
Chethan Lakshman Bagi <bagichethan@gmail.com>
2026-08-16 22:31:51 +0530

    Dqueue question  example

---
+++ src/Stack_1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Stack_1{
+    public static void main(String args[]){
+
+        Deque<Integer> Stack= new ArrayDeque<>();
+
+        Stack.push(10);
+        Stack.push(20);
+        Stack.push(30);
+
+        System.out.println(Stack.peek());
+        System.out.println(Stack.pop());
+        System.out.println(Stack.peek());
+
+        System.out.println(Stack.isEmpty());
+    }
+}
+
+
+
